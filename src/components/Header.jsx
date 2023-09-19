9a07ece98cc8f9ee910010cabc1b47385ae4bf69
Amol <shelkeamol897@gmail.com>
2023-09-19 23:45:02 +0530

    [feat]: made navbar responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import { navLinks } from "@/libs/constants";
 import ListItem from "./ListItem";
 import Link from "next/link";
-// import { BiArrowToBottom } from "react-icons/bi";
 import { MdOutlineShoppingBag } from "react-icons/md";
+import { AiOutlineMenu } from "react-icons/ai";
+import { useState } from "react";
 
 export const Header = () => {
+  const [navActive, setNavActive] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="logo-container">
@@ -14,11 +17,14 @@ export const Header = () => {
         </Link>
       </div>
       <div>
-        <ul className="nav-links">
+        <ul className={`nav-links ${navActive ? "active" : ""}`}>
           {navLinks.map((item) => (
             <ListItem url={item.url} key={item.id} title={item.title} />
           ))}
         </ul>
+        <div className="menu" onClick={() => setNavActive(!navActive)}>
+          <AiOutlineMenu size={20} />
+        </div>
       </div>
     </nav>
   );
